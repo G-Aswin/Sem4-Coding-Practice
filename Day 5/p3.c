9d3c1404166e72ca54137f361ed679539dc14b0c
G-Aswin <aswin.g.rns@gmail.com>
2021-04-26 17:19:23 +0530

    day 5 p3

--- Day 5/p3.c
+++ Day 5/p3.c
@@ -1,5 +1,12 @@
-#include <string.h>
 #include <stdio.h>
+
+int stringlength(char arr[])
+{
+    int l = 0;
+    while (arr[l] != '\0')
+        l++;
+    return l;   
+}
 
 int main()
 {
@@ -8,7 +15,7 @@ int main()
     gets(str);
 
     printf("\nThe individual words are ->\n");
-    for (int i = 0; i < strlen(str); i++)
+    for (int i = 0; i < stringlength(str); i++)
     {
         if (str[i] != ' ')
             printf("%c", str[i]);
